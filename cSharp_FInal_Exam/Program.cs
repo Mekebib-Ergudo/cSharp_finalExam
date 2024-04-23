7082cf44242665113844fbecde546ce5df8f8111
Maki <Maki@DESKTOP-R1V30B2>
2024-04-22 22:17:15 -0400

    Add project files.

---
+++ cSharp_FInal_Exam/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace cSharp_FInal_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
