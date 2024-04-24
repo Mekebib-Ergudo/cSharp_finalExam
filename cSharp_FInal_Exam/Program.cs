6681789be0ee398678f991b4d548babe50cdb240
Maki <Maki@DESKTOP-R1V30B2>
2024-04-23 20:49:55 -0400

    Q 10

--- cSharp_FInal_Exam/Program.cs
+++ cSharp_FInal_Exam/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace cSharp_FInal_Exam
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace cSharp_FInal_Exam
 {
     internal class Program
     {
@@ -6,6 +9,56 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello Git");
+
+            // Example usage of GenericClass
+            GenericClass<int> genericClass = new GenericClass<int>(5);
+            genericClass.Add(1);
+            genericClass.Add(2);
+            genericClass.Add(3);
+
+            var numbers = genericClass.GetNumbers(genericClass.GetCollection());
+            foreach (var num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+
+    public class GenericClass<T>
+        where T : struct 
+    {
+        private Collection<T> _collection;
+
+        public GenericClass(int number)
+        {
+            _collection = new Collection<T>();
+        }
+
+        public void Add(T value)
+        {
+            _collection.Add(value);
+        }
+
+        public T[] GetNumbers(Collection<T> collections)
+        {
+            T[] numbers = new T[collections.Count];
+            int i = 0;
+            foreach (T value in collections)
+            {
+                numbers[i++] = value;
+            }
+            return numbers;
+        }
+
+        public Collection<T> GetCollection()
+        {
+            Collection<T> collection = new Collection<T>();
+
+            for (int i = _collection.Count - 1; i >= 0; i--)
+            {
+                collection.Add(_collection[i]);
+            }
+            return collection;
         }
     }
 }
